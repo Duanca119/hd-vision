b8de6dd47fb3cf0965339e9709450f214d1ddba7
Z User <z@container>
2026-03-30 19:08:18 +0000

    fix: limpiar dependencias innecesarias y fix build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  allowedDevOrigins: [
-    "*",
-  ],
+  output: 'standalone',
 };
 
 export default nextConfig;
